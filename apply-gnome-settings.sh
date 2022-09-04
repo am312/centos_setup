808a08050ca1821d1186c0206d34a6895783ccbe
Andrew Munn <am312@users.noreply.github.com>
2022-09-04 15:33:44 -0400

    Create apply-gnome-settings.sh

---
+++ apply-gnome-settings.sh
@@ -0,0 +1,5 @@
+#!/bin/bash
+
+gsettings set org.gnome.desktop.session idle-delay 0
+gsettings set org.gnome.desktop.lockdown disable-lock-screen true
+gsettings set org.gnome.desktop.wm.preferences button-layout ":minimize,maximize,close"
