2a47efe68157260b10d7fb3427bf703fadb73f74
Andrew Munn <am312@users.noreply.github.com>
2022-09-04 15:50:55 -0400

    Update apply-gnome-settings.sh

--- apply-gnome-settings.sh
+++ apply-gnome-settings.sh
@@ -3,3 +3,7 @@
 gsettings set org.gnome.desktop.session idle-delay 0
 gsettings set org.gnome.desktop.lockdown disable-lock-screen true
 gsettings set org.gnome.desktop.wm.preferences button-layout ":minimize,maximize,close"
+
+# Set the background to a solid color - pea green
+gsettings set org.gnome.desktop.background picture-uri ''
+gsettings set org.gnome.desktop.background primary-color '#808000'
