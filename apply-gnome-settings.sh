0591419f7fc76eac57a6cc5c653b9c7ae4420200
Andrew Munn <am312@users.noreply.github.com>
2022-09-06 10:16:52 -0400

    Update apply-gnome-settings.sh

--- apply-gnome-settings.sh
+++ apply-gnome-settings.sh
@@ -1,7 +1,10 @@
 #!/bin/bash
 
+# Never blank/lock the screen.  Good if running in a VM.
 gsettings set org.gnome.desktop.session idle-delay 0
 gsettings set org.gnome.desktop.lockdown disable-lock-screen true
+
+# Put the window controls back where they should be.
 gsettings set org.gnome.desktop.wm.preferences button-layout ":minimize,maximize,close"
 
 # Set the background to a solid color - pea green
