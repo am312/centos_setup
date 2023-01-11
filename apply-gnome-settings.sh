307f2207d16b79bcac74ffeca19ce3252a6282ca
Andrew Munn <am312@users.noreply.github.com>
2023-01-10 22:05:31 -0500

    Update apply-gnome-settings.sh

--- apply-gnome-settings.sh
+++ apply-gnome-settings.sh
@@ -7,6 +7,6 @@ gsettings set org.gnome.desktop.lockdown disable-lock-screen true
 # Put the window controls back where they should be.
 gsettings set org.gnome.desktop.wm.preferences button-layout ":minimize,maximize,close"
 
-# Set the background to a solid color - pea green
+# Set the background to a solid color - plum
 gsettings set org.gnome.desktop.background picture-uri ''
-gsettings set org.gnome.desktop.background primary-color '#808000'
+gsettings set org.gnome.desktop.background primary-color '#502566'
